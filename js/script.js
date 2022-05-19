4a091e08949873ee35f855c7ecafc1266c9160d5
craigbauer89 <craigbauer89@gmail.com>
2022-05-19 18:21:13 +0200

    Craig push

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ function showSlides(n) {
 
       var windowheight = window.innerHeight;
       var revealtop = reveals[i].getBoundingClientRect().top;
-      var revealpoint = 150;
+      var revealpoint = 50;
 
       if(revealtop < windowheight - revealpoint){
         reveals[i].classList.add('active');
